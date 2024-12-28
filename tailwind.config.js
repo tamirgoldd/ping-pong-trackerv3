6f0628d66de891773476983fe214b0ced6b01df3
tamirgoldd <tamir.gold@darrow.ai>
2024-12-28 21:43:25 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          400: '#c084fc',
+          500: '#a855f7',
+          600: '#9333ea',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
